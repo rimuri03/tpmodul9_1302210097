6ee60b5652840847307332142a7dd27d54dccd8a
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2023-04-16 21:38:44 +0700

    menambahkan API mahasiswa

---
+++ tpmodul9_1302210097/Controllers/MahasiswaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace tpmodul9_1302210097.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController: ControllerBase
+    {
+        public static List<Mahasiswa> ListMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa ("Rizki Muhammad Ridwan","1302210097"),
+            new Mahasiswa ("Muhammad Zaky Fathurahim","1302213067"),
+            new Mahasiswa ("Reza Adhie Dharmawan","1302213016"),
+            new Mahasiswa ("Mochaman Rifqi Barlian","1302210125"),
+            new Mahasiswa ("Raihan Sulthon Yaafi","1302213074"),
+            new Mahasiswa ("Fasya Rayhan Maulana","1302213116" )
+        };
+   
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return ListMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            if (id < 0 || id >= ListMahasiswa.Count)
+            {
+                NotFound();
+            }
+            return ListMahasiswa[id];
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            ListMahasiswa.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id < 0 || id >= ListMahasiswa.Count)
+            {
+                NotFound();
+            }
+            ListMahasiswa.RemoveAt(id);
+        }
+
+   
+    }
+}
---
+++ tpmodul9_1302210097/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace tpmodul9_1302210097
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; private set; }
+        public string Nim { get; private set; }
+
+        public Mahasiswa(string Nama, string Nim)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+        }
+    }
+}
